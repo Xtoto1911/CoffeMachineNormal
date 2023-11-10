8b68c68473f0fc3163ade56339e3e9a3ae9aa7b2
Xtoto1911 <super.mr.kirill@mail.ru>
2023-11-11 02:16:04 +0300

    исправил ошибки

--- CoffeMachineNew/Win/CoffMach.xaml.cs
+++ CoffeMachineNew/Win/CoffMach.xaml.cs
@@ -118,9 +118,11 @@ namespace CoffeMachineNew.Win
             if (!Directory.Exists(Environment.CurrentDirectory + "/Resources"))
                 Directory.CreateDirectory(Environment.CurrentDirectory + "/Resources");
             if (!File.Exists(Environment.CurrentDirectory + $"/Resources/{PathName}Drinks.json"))
-                File.Create(Environment.CurrentDirectory + $"/Resources/{PathName}Drinks.json");
+            {
+                using (File.Create(Environment.CurrentDirectory + $"/Resources/{PathName}Drinks.json")) { }
+            }
             if (!File.Exists(Environment.CurrentDirectory + $"/Resources/{PathName}Topings.json"))
-                File.Create(Environment.CurrentDirectory + $"/Resources/{PathName}Topings.json");
+                using (File.Create(Environment.CurrentDirectory + $"/Resources/{PathName}Topings.json")) { }
         }
 
         private void AdminBtn_Click(object sender, RoutedEventArgs e)//админ панель
